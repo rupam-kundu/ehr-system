2b974c09ef3b8e73db959ee161df38da3e6fd228
Rupam Kundu <rupamkundu1000000@gmail.com>
2021-11-14 21:43:42 +0530

    Designed the dashboard.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,89 @@
-//import logo from "./logo.svg";
-import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Switch, Route, Link } from "react-router-dom";
 import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 import Amplify from "aws-amplify";
 import awsExports from "./aws-exports";
-
+import PatientsList from "./components/patients-list";
 Amplify.configure(awsExports);
 
 function App() {
   return (
-    <div className="jumbotron">
-      <h1 className="display-4" style={{ textAlign: "center" }}>
-        EHR system
-      </h1>
-      <AmplifySignOut />
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a
+          href="/"
+          className="navbar-brand"
+          style={{ marginLeft: "45%" }}
+        >
+          <h1>EHR system</h1>
+        </a>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item" style={{marginLeft: "220%"}}>
+            <AmplifySignOut />
+          </li>
+        </div>
+      </nav>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-2">
+            <div>
+              <ul class="nav flex-column">
+                <li class="nav-item">
+                  <div class="card" style={{width: "18rem"}}>
+                    <div class="card-body">
+                      <Link to={"/patients"} className="nav-link">
+                        <h5 class="card-title">View patients</h5>
+                      </Link>
+                    </div>
+                  </div>
+                </li>
+                <li class="nav-item">
+                <div class="card" style={{width: "18rem"}}>
+                    <div class="card-body">
+                      <Link to={""} className="nav-link">
+                        <h5 class="card-title">Link</h5>
+                      </Link>
+                    </div>
+                  </div>
+                </li>
+                <li class="nav-item">
+                <div class="card" style={{width: "18rem"}}>
+                    <div class="card-body">
+                      <Link to={""} className="nav-link">
+                        <h5 class="card-title">Link</h5>
+                      </Link>
+                    </div>
+                  </div>
+                </li>
+                <li class="nav-item">
+                <div class="card" style={{width: "18rem"}}>
+                    <div class="card-body">
+                      <Link to={""} className="nav-link">
+                        <h5 class="card-title">Link</h5>
+                      </Link>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-10">
+            <div class="row">
+              <div class="col-10">
+                <div>
+                  <Switch>
+                    <Route
+                      exact
+                      path={["/", "/patients"]}
+                      component={PatientsList}
+                    />
+                  </Switch>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/patients-list.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const PatientsList = (props) => {
+  return (
+    <div>
+      <h2 style={{textAlign: "center", marginTop: "1rem"}}>Patients</h2>
+    </div>
+  );
+};
+
+export default PatientsList;
