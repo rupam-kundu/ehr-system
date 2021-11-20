a292106a4e3590106ee3bf9464b0b1797bd338e3
Rupam Kundu <rupamkundu1000000@gmail.com>
2021-11-20 22:27:05 +0530

    Designed the sidebar.

--- src/App.js
+++ src/App.js
@@ -10,56 +10,67 @@ function App() {
   return (
     <div>
       <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <a
-          href="/"
-          className="navbar-brand"
-          style={{ marginLeft: "45%" }}
-        >
+        <a href="/" className="navbar-brand" style={{ marginLeft: "45%" }}>
           <h1>EHR system</h1>
         </a>
         <div className="navbar-nav mr-auto">
-          <li className="nav-item" style={{marginLeft: "220%"}}>
+          <li className="nav-item" style={{ marginLeft: "220%" }}>
             <AmplifySignOut />
           </li>
         </div>
       </nav>
       <div class="container-fluid">
         <div class="row">
-          <div class="col-2">
+          <div class="col-2" style={{ paddingLeft: "0%" }}>
             <div>
               <ul class="nav flex-column">
                 <li class="nav-item">
-                  <div class="card" style={{width: "18rem"}}>
+                  <div
+                    class="card text-white bg-dark"
+                    style={{width: "18rem", borderRadius: "0%"}}
+                  >
                     <div class="card-body">
                       <Link to={"/patients"} className="nav-link">
-                        <h5 class="card-title">View patients</h5>
+                        <h5 class="card-title" style={{ color: "white" }}>
+                          View patients
+                        </h5>
                       </Link>
                     </div>
                   </div>
                 </li>
                 <li class="nav-item">
-                <div class="card" style={{width: "18rem"}}>
+                  <div
+                    class="card text-white bg-dark"
+                    style={{ width: "18rem", borderRadius: "0%" }}
+                  >
                     <div class="card-body">
                       <Link to={""} className="nav-link">
-                        <h5 class="card-title">Link</h5>
+                        <h5 class="card-title" style={{ color: "white" }}>
+                          Link
+                        </h5>
                       </Link>
                     </div>
                   </div>
                 </li>
                 <li class="nav-item">
-                <div class="card" style={{width: "18rem"}}>
+                  <div
+                    class="card text-white bg-dark"
+                    style={{ width: "18rem", borderRadius: "0%" }}
+                  >
                     <div class="card-body">
                       <Link to={""} className="nav-link">
-                        <h5 class="card-title">Link</h5>
+                        <h5 class="card-title" style={{ color: "white" }}>
+                          Link
+                        </h5>
                       </Link>
                     </div>
                   </div>
                 </li>
                 <li class="nav-item">
-                <div class="card" style={{width: "18rem"}}>
+                  <div class="card text-white bg-dark" style={{width: "18rem", borderRadius: "0%"}}>
                     <div class="card-body">
                       <Link to={""} className="nav-link">
-                        <h5 class="card-title">Link</h5>
+                        <h5 class="card-title" style={{ color: "white" }}>Link</h5>
                       </Link>
                     </div>
                   </div>
